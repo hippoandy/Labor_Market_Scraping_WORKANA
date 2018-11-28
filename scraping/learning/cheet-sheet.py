5b5269a5176a8f58a0428c08eff618b64aaef035
hippoandy <ycaho@ucdavis.edu>
2018-11-28 14:33:57 -0800

    Notes

---
+++ scraping/learning/cheet-sheet.py
@@ -0,0 +1,11 @@
+
+# to install additional library on macOS to Jupyter
+'''
+pip install pandas ipython[all] jupyter
+'''
+
+# ref. https://nikgrozev.com/2015/12/27/pandas-in-jupyter-quickstart-and-useful-snippets/
+
+# Dataframe operations
+# Ref. https://thispointer.com/python-pandas-how-to-add-rows-in-a-dataframe-using-dataframe-append-loc-iloc/
+
